4498e0a8c28e71c728e9f46e522d2f64268cb715
Muhammed Humam Hossain <66829442+humam-hossain@users.noreply.github.com>
2023-09-14 22:47:21 +0600

    implemeting xor training

--- project-ml/nn.h
+++ project-ml/nn.h
@@ -1,9 +1,10 @@
 #ifndef NN_H_
 #define NN_H_
 
-#define MAT_AT(m, i, j) (m).es[(i)*(m).cols + (j)]
+#define MAT_AT(m, i, j) (m).es[(i)*(m).stride + (j)]
 
 #include <stdio.h>
+#include <math.h>
 
 #ifndef NN_MALLOC
 #include <stdlib.h>
@@ -18,17 +19,23 @@
 typedef struct {
 	size_t rows;
 	size_t cols;
-	float* es;
 	size_t stride;
+	float* es;
 } Mat;
 
 float rand_float();
+float sigmoidf(float x);
+
 Mat mat_alloc(size_t rows, size_t cols);
 void mat_fill(Mat a, float x);
 void mat_rand(Mat m, float low = 0, float high = 1);
+Mat mat_row(Mat m, size_t  row);
+void mat_copy(Mat dst, Mat src);
 void mat_dot(Mat dst, Mat a, Mat b);
 void mat_sum(Mat dst, Mat a);
-void mat_print(Mat m);
+void mat_sig(Mat m);
+void mat_print(Mat m, const char* name);
+#define MAT_PRINT(m) mat_print(m, #m)
 
 #endif // NN_H_
 
@@ -39,11 +46,17 @@ float rand_float()
 	return (float)rand() / (float)RAND_MAX;
 }
 
+float sigmoidf(float x)
+{
+	return 1.0f / (1.0f + expf(-x));
+}
+
 Mat mat_alloc(size_t rows, size_t cols) 
 {
 	Mat m;
 	m.rows = rows;
 	m.cols = cols;
+	m.stride = cols;
 	m.es = (float *)NN_MALLOC(sizeof(*m.es) * rows * cols);
 	NN_ASSERT(m.es != nullptr);
 	return m;
@@ -63,6 +76,28 @@ void mat_rand(Mat m, float low, float high)
 	for (size_t i = 0; i < m.rows; ++i) {
 		for (size_t j = 0; j < m.cols; ++j) {
 			MAT_AT(m, i, j) = rand_float() * (high - low) + low;
+		}
+	}
+}
+
+Mat mat_row(Mat m, size_t  row)
+{
+	Mat r;
+	r.rows = 1;
+	r.cols = m.cols;
+	r.stride = m.stride;
+	r.es = &MAT_AT(m, row, 0);
+	
+	return r;
+}
+void mat_copy(Mat dst, Mat src)
+{
+	NN_ASSERT(dst.rows == src.rows);
+	NN_ASSERT(dst.cols == src.cols);
+
+	for (size_t i = 0; i < dst.rows; ++i) {
+		for (size_t j = 0; j < dst.cols; ++j) {
+			MAT_AT(dst, i, j) = MAT_AT(src, i, j);
 		}
 	}
 }
@@ -94,14 +129,26 @@ void mat_sum(Mat dst, Mat a)
 		}
 	}
 }
-void mat_print(Mat m)
+
+void mat_sig(Mat m)
 {
+	for (size_t i = 0; i < m.rows; ++i) {
+		for (size_t j = 0; j < m.cols; ++j) {
+			MAT_AT(m, i, j) = sigmoidf(MAT_AT(m, i, j));
+		}
+	}
+}
+
+void mat_print(Mat m, const char* name)
+{
+	printf("%s = [\n", name);
 	for (size_t i = 0; i < m.rows; ++i) {
 		for (size_t j = 0; j < m.cols; ++j) {
 			printf("%f ", MAT_AT(m, i, j));
 		}
 		printf("\n");
 	}
+	printf("]\n");
 }
 
 #endif // NN_IMPLEMENTATION
